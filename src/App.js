783830c32441d7eee7e9fe41cf1735ed6bd2cdbd
Chris-Frey <chris.frey@live.com>
2023-07-18 14:14:34 -0700

    uncommented code after testing functionality

--- src/App.js
+++ src/App.js
@@ -1,24 +1,23 @@
 import { Routes, Route } from "react-router-dom"
 import { useState } from "react";
-import logo from './logo.svg';
 import './App.css';
 import Header from './pages/Header'
 
 function App() {
 
-  // const [users, setUsers] = useState(mockUsers)
+  const [users, setUsers] = useState(mockUsers)
   return (
       <>
       <Header />
       <Routes>
-        {/* <Route path="/" element={<Home />} />
+        <Route path="/" element={<Home />} />
         <Route path="/signup" element={<SignUp />} />
         <Route path="/activityindex" element={<ActivityIndex />} />
         <Route path="/activityshow" element={<ActivityShow />} />
         <Route path="/activitynew" element={<ActivityNew />} />
         <Route path="/activityedit" element={<ActivityEdit />} />
         <Route path="/buddyprofile" element={<BuddyProfile />} />
-        <Route path="*" element={<NotFound />} /> */}
+        <Route path="*" element={<NotFound />} />
       </Routes>
 
       
