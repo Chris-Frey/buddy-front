3860e957e4eaf3065cb48415ee9bfc29ef937d41
Chris-Frey <chris.frey@live.com>
2023-07-19 09:55:21 -0700

    removed unneccesary modal files

--- src/App.js
+++ src/App.js
@@ -13,13 +13,13 @@ function App() {
       <Header />
       <Routes>
         <Route path="/" element={<Home />} />
-        {/* <Route path="/signup" element={<SignUp />} />
+        <Route path="/signup" element={<SignUp />} />
         <Route path="/activityindex" element={<ActivityIndex />} />
         <Route path="/activityshow" element={<ActivityShow />} />
         <Route path="/activitynew" element={<ActivityNew />} />
         <Route path="/activityedit" element={<ActivityEdit />} />
         <Route path="/buddyprofile" element={<BuddyProfile />} />
-        <Route path="*" element={<NotFound />} /> */}
+        <Route path="*" element={<NotFound />} />
       </Routes>
 
       
