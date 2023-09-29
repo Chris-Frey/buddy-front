957df3df20b82b2289b2b3197a7446c820802e14
Chris Frey <chris.frey@hey.com>
2023-09-28 22:58:37 -0700

    login now working, not sure why it wasn't...

--- src/pages/LogIn.js
+++ src/pages/LogIn.js
@@ -1,11 +1,10 @@
 import React, { useRef, useState } from 'react'
-import { Input, Spacer, Button, Grid } from "@nextui-org/react";
-import { NavLink, useNavigate } from 'react-router-dom'
+// import { Input, Button } from "@nextui-org/react";
+import { NavLink } from 'react-router-dom'
 import styles from "../styles/LogIn.css"
 
 const LogIn = ({login}) => {
   const formRef = useRef()
-  const navigate = useNavigate()
 
   const handleSubmit = (e) => {
     e.preventDefault()
@@ -18,20 +17,10 @@ const LogIn = ({login}) => {
       "user":{ email: data.email, password: data.password}
     }
     login(userInfo)
-    navigate("/")
-  }
-
-  const [userLogin, setUserLogin] = useState({
-    category:"",
-    activity_name:"",
-  })
-
-  const handleChange = (e) => {
-    setUserLogin({...userLogin, [e.target.name]: e.target.value})
   }
 
   return (
-    <>
+  <>
     <div className="title_logo">
     <img src="/logo512.png" alt='Whim Logo'/>
     </div>
@@ -41,31 +30,21 @@ const LogIn = ({login}) => {
       onSubmit={handleSubmit}
     >
       <input
-        onChange={handleChange}
         type='email'
         name='email'
         placeholder='Email'
       />
-      <input onChange={handleChange} type='password' name='password' placeholder='Password'/>
-
+      <input
+        type='password'
+        name='password'
+        placeholder='Password'
+      />
       <button type='submit' onClick={handleSubmit}>Log In</button>
-
-    <NavLink to="/signup">
-      <button>Sign Up</button>
-    </NavLink>
+      <NavLink to="/signup">
+        <button>Sign Up</button>
+      </NavLink>
     </form>
 
-{/* nextUI form */}
-    <form ref={formRef} onSubmit={handleSubmit}>
-        <Input  labelPlaceholder="Email" type='email' name='email'/>
-        <Input  labelPlaceholder="Password" type='password' name='password'/>
-        <NavLink to="/" >
-          <Button
-          type='submit'>
-            Log In
-          </Button>
-        </NavLink>
-    </form>
   </>
   )
 }
